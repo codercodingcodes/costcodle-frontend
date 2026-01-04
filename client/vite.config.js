b1938eb1b0ddaef66c5ee8f981d6892d04ccb5ab
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-04 03:59:45 -0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
-
+console.log("testing")
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
