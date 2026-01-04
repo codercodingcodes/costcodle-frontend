10d627c1f84f7582ed6a885cf32fc4c90fea39db
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-04 00:01:42 -0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -21,6 +21,17 @@ export default defineConfig({
           "Access-Control-Allow-Credentials": true,
           "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT",
           "Access-Control-Allow-Headers":"*"},
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
       },
     },
     hmr: {
