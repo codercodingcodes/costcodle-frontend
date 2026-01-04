b49c1b20ce69768b9392f3f9882398287a134733
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-04 03:57:27 -0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,33 +10,19 @@ export default defineConfig({
   envDir: '../',
   server: {
     proxy: {
-      '/api': {
-        // target: 'costcodle.vercel.app',
-        target: 'https://costcodle-321020937506.europe-west1.run.app',
-        changeOrigin: true,
-        secure: true,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        // headers:{"Access-Control-Allow-Origin":"*",
-          // "Access-Control-Allow-Credentials": true,
-          // "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT",
-          // "Access-Control-Allow-Headers":"*"},
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      },
+      // '/api': {
+      //   // target: 'costcodle.vercel.app',
+      //   target: 'https://costcodle-321020937506.europe-west1.run.app',
+      //   changeOrigin: true,
+      //   secure: true,
+      //   ws: true,
+      //   rewrite: (path) => path.replace(/^\/api/, ''),
+      // },
+      '/api': 'https://costcodle-321020937506.europe-west1.run.app'
     },
-    hmr: {
-      clientPort: 443,
-    },
-    allowedHosts: true,
+    // hmr: {
+    //   clientPort: 443,
+    // },
+    // allowedHosts: true,
   }
 });
