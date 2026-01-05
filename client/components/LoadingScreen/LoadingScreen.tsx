57cb6f8789e168d1136b34ccbe93b044436efbda
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-05 00:50:38 -0800

    Update LoadingScreen.tsx

--- client/components/LoadingScreen/LoadingScreen.tsx
+++ client/components/LoadingScreen/LoadingScreen.tsx
@@ -9,7 +9,7 @@ function LoadingScreen({progress}:{progress:number}) {
             {/*<div className={"absolute sm:w-1/3 md:w-1/12 top-1/3 left-1/2 -translate-x-1/2 "}>*/}
             {/*    <img src={loading} className={" animate-spin w-full h-full"} />*/}
             {/*</div>*/}
-            <p className={"absolute font-costco text-red-600 text-4xl top-1/2 left-1/2 -translate-x-1/2"}>loading</p>
+            <p className={"absolute font-costco text-red-600 text-4xl top-1/2 left-1/2 -translate-x-1/2 animate-fade"}>loading</p>
 
         </div>
     );
