aa1eb9372c272e93caf1a35086c2dae4d3ebc6a0
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-05 00:49:29 -0800

    changed animation

--- client/components/LoadingScreen/LoadingScreen.tsx
+++ client/components/LoadingScreen/LoadingScreen.tsx
@@ -9,11 +9,7 @@ function LoadingScreen({progress}:{progress:number}) {
             {/*<div className={"absolute sm:w-1/3 md:w-1/12 top-1/3 left-1/2 -translate-x-1/2 "}>*/}
             {/*    <img src={loading} className={" animate-spin w-full h-full"} />*/}
             {/*</div>*/}
-            <p className={"absolute font-costco text-red-600 text-4xl top-2/3 left-1/2 -translate-x-1/2"}>loading</p>
-            <div className={"h-1 w-2/3 sm:w-1/3 bg-gray-300"}>
-                <div style={{width:`${progress/3}%`}} className={`h-full`}>
-                </div>
-            </div>
+            <p className={"absolute font-costco text-red-600 text-4xl top-1/2 left-1/2 -translate-x-1/2"}>loading</p>
 
         </div>
     );
