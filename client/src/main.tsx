0cd31cdabfd6f4d95ed73407ed6a584858986d25
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-05 15:25:46 -0800

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-    dsn: "https://b57458227a52237b9a973fa466c31d14@o4510660094787584.ingest.us.sentry.io/4510660099112960",
+    dsn: "/log",
     // Setting this option to true will send default PII data to Sentry.
     // For example, automatic IP address collection on events
     sendDefaultPii: true,
