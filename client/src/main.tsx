aca646bc732a6f05388e818c3581a91545c5e10c
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-05 15:03:06 -0800

    readded sentry

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://b57458227a52237b9a973fa466c31d14@o4510660094787584.ingest.us.sentry.io/4510660099112960",
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
