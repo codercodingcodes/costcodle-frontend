e42b90a53f988a490aaed4b2caedf950bc6e3e42
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-06 03:54:37 -0800

    Update vercel.ts

--- client/vercel.ts
+++ client/vercel.ts
@@ -3,7 +3,7 @@ import { routes, type VercelConfig } from '@vercel/config/v1';
 export const config: VercelConfig = {
     buildCommand: 'npm run build',
     rewrites: [
-        // routes.rewrite('/api/(.*)', 'https://costcodle.vercel.app/$1'),
-        routes.rewrite('/api/(.*)', 'costcodle.vercel.app/$1'),
+        routes.rewrite('/api/(.*)', 'https://costcodle.vercel.app/$1'),
+        // routes.rewrite('/api/(.*)', 'https://costcodle-321020937506.europe-west1.run.app/$1'),
     ],
 };
