72c0374db3e7585c5787862ab20437579f5881f3
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-05 17:19:49 -0800

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,10 @@ import './main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as Sentry from "@sentry/react";
-import {patchUrlMappings} from "@discord/embedded-app-sdk";
-patchUrlMappings([{prefix: '/log', target: 'https://b57458227a52237b9a973fa466c31d14@o4510660094787584.ingest.us.sentry.io/4510660099112960'}]);
+
 Sentry.init({
     dsn:"https://b57458227a52237b9a973fa466c31d14@o4510660094787584.ingest.us.sentry.io/4510660099112960",
-    tunnel:"https://1445980061390999564.discordsays.com/log",
+    tunnel:"/api/log",
     // Setting this option to true will send default PII data to Sentry.
     // For example, automatic IP address collection on events
     sendDefaultPii: true,
