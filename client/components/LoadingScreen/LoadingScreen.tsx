adf9362843421a0698b319d68e8f6f370216f370
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-07 13:19:18 -0800

    updated ui

--- client/components/LoadingScreen/LoadingScreen.tsx
+++ client/components/LoadingScreen/LoadingScreen.tsx
@@ -7,7 +7,7 @@ function LoadingScreen() {
     return (
         <div className="w-full h-full text-center">
             <p className={"absolute font-costco text-red-600 text-4xl top-1/2 left-1/2 -translate-x-1/2 animate-fade"}>loading...</p>
-
+            <p>Beta Version. Please contact us with any issues you've encountered in the support server</p>
         </div>
     );
 }
