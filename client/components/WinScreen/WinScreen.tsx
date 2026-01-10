954116c9dbf49d6a7e3482693e9a99fddcdd1f0f
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-10 14:15:36 -0800

    Update WinScreen.tsx

--- client/components/WinScreen/WinScreen.tsx
+++ client/components/WinScreen/WinScreen.tsx
@@ -7,7 +7,7 @@ function WinScreen({guessCnt,time,toggle}:{guessCnt:number,time:number,toggle:()
     return (
         <div className="w-full h-full text-center">
             <p className={"absolute font-costco text-red-600 text-4xl top-1/2 left-1/2 -translate-x-1/2"}>Congrats! You got today's item in {guessCnt} tries!</p>
-            <p className={"absolute font-costco text-red-600 text-4xl top-1/2 left-1/2 -translate-x-1/2"}>Next game in : {24-Math.floor(time / 3600)} hrs</p>
+            <p className={"absolute font-costco  text-2xl top-3/4 left-1/2 -translate-x-1/2"}>Next game in : {24-Math.floor(time / 3600)} hrs</p>
             <button className={"fixed top-[60px] md:top-10 right-[10px] text-3xl bg-red-500 p-1 border-red-700 border-solid border-3 rounded-lg h-[30px]"} onClick={toggle}>
                 <p className={"-translate-y-[14px]"}>
                     x
