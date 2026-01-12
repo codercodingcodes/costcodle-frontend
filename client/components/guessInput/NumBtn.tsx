91a7bdb62dc8d6795dc157ba57a2da85e55ba569
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-12 01:40:03 -0800

    Update NumBtn.tsx

--- client/components/guessInput/NumBtn.tsx
+++ client/components/guessInput/NumBtn.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function NumBtn({num,value,setVal}: {num: string,value:number,setVal:(x:number) => void}) {
     return (
         <div className="NumBtn h-full p-1 font-bold font-sans">
-            <button className={"w-full h-full text-center bg-gray-400 hover:bg-gray-500 rounded-xl "} onClick={()=>setVal(value)}>{num}</button>
+            <button className={"w-full h-full text-center bg-gray-300 hover:bg-gray-400 rounded-xl "} onClick={()=>setVal(value)}>{num}</button>
         </div>
     );
 }
