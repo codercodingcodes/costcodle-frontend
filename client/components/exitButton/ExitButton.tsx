f1629307e117cfb802a6fcb7ce4b6de322bd0574
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-13 15:34:18 -0800

    Update ExitButton.tsx

--- client/components/exitButton/ExitButton.tsx
+++ client/components/exitButton/ExitButton.tsx
@@ -4,8 +4,8 @@ import React  from 'react';
 import xIcon from '../../resources/75519.png'
 function StatBar({toggle}:{toggle:()=>void}) {
     return (
-        <button className={"flex flex-row fixed top-[60px] md:top-10 right-[10px] text-3xl h-[30px]"} onClick={toggle}>
-            <p className={"h-full text-lg"}>Return to game</p>
+        <button className={"flex flex-row fixed top-[60px] md:top-10 right-[20px] text-3xl h-[30px]"} onClick={toggle}>
+            <p className={"m-auto text-center text-lg"}>Return to game</p>
             <img src={xIcon} className={"h-[20px] w-[20px]"}/>
         </button>
     );
