51cb28c2f58ff910bde0b6f8a90f401fc14edade
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-13 17:35:33 -0800

    Update InfoPanel.tsx

--- client/components/infoPanel/InfoPanel.tsx
+++ client/components/infoPanel/InfoPanel.tsx
@@ -8,22 +8,13 @@ function InfoPanel({toggle}:{toggle:() => void }) {
            <ExitButton toggle={toggle}/>
             <div className={"wrap-break-word overflow-scroll text-center h-full w-full font-sans"}>
                 <p className={"mt-[10px]"}>
-                    Guess the price($USD) of the Costco item listed
+                    Guess the price($USD) of the Costco item listed in 5 guesses
                 </p>
                 <p className={"mt-[10px]"}>
                     <a className={"bg-red-600"}>RED</a> means that your guess is within 10% of the price
                 </p>
                 <p className={"mt-[10px] w-full border-gray-400 border-b-2 border-solid m-auto pb-5" }>
                     <a className={"bg-yellow-300"}>YELLOW</a> means that your guess is over 10% of the price
-                </p>
-                <p className={"mt-5"}>
-                    You get 5 guesses per day to guess the price of the item
-                </p>
-                <p className={"mt-[10px]"}>
-                    The game will tell you how close your guess is to the actual price via color
-                </p>
-                <p className={"mt-[10px] w-full border-gray-400 border-b-2 border-solid m-auto pb-5"}>
-                    The item changes daily and you can see the performance of other users in your channel when you click on the "Friends" in the header
                 </p>
                 <p className={"mt-5"}>
                     DISCLOSURE!! This game was NOT invented by me but by @KermWasTaken and I merely adapted it to the discord format.
