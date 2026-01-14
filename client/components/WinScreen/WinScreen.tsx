be077ad348770246f88e886de91f0839e435f17e
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-13 21:52:37 -0800

    Update WinScreen.tsx

--- client/components/WinScreen/WinScreen.tsx
+++ client/components/WinScreen/WinScreen.tsx
@@ -10,8 +10,8 @@ function WinScreen({guessCnt,gameData,toggle,users,self}:{guessCnt:number,gameDa
     return (
         <div className="top-0 w-full h-full text-center bg-gray-100 fixed animate-fade-in-fast">
             <StatBar users={users} self={self} toggle={toggle}/>
-            <p className={"absolute font-costco text-red-600 text-2xl md:text-4xl icon:top-1/3 md:top-3/5 left-1/2 -translate-x-1/2 w-full"}>Congrats! You got today's item in {guessCnt} tries!</p>
-            <p className={"absolute font-costco text-xl md:text-2xl icon:top-1/2 md:top-4/5 left-1/2 -translate-x-1/2 w-full"}>Next game in : {24-Math.floor(gameData.time / 3600)} hrs</p>
+            <p className={"absolute font-costco text-red-600 text-2xl md:text-4xl top-2/3 left-1/2 -translate-x-1/2 w-full"}>Congrats! You got today's item in {guessCnt} tries!</p>
+            <p className={"absolute font-costco text-xl md:text-2xl top-4/5 left-1/2 -translate-x-1/2 w-full"}>Next game in : {24-Math.floor(gameData.time / 3600)} hrs</p>
         </div>
     );
 }
