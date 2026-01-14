2c6cea3c3e8cd8b7f593d8590564f1d77c3d226d
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-14 13:35:11 -0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -430,7 +430,7 @@ function App() {
     }
     return (
       <div className={"bg-gray-200"}>
-          <img className={"w-full h-full fixed arbitrary:block icon:hidden z-20"} src={logo}/>
+          <img className={"w-full h-full fixed z-20 [@media(height<300px)]:block [@media(height>300px)]:hidden"} src={logo}/>
           {statbar && gameInfo && users.length>1
               ?
               <div className={"w-full h-full animate-fade-in-fast"} key={statbarKey}>
