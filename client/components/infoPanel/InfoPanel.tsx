648c9d4a9dc5f57a201baa80f9d8122fa0759575
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-13 18:52:52 -0800

    updated info

--- client/components/infoPanel/InfoPanel.tsx
+++ client/components/infoPanel/InfoPanel.tsx
@@ -4,31 +4,31 @@ import ExitButton from "../exitButton/ExitButton";
 
 function InfoPanel({toggle}:{toggle:() => void }) {
     return (
-        <div className="fixed w-full h-full bg-gray-100 pt-20 md:pt-0 top-[50px]">
-           <ExitButton toggle={toggle}/>
-            <div className={"wrap-break-word overflow-scroll text-center h-full w-full font-sans"}>
-                <p className={"mt-[10px]"}>
-                    Guess the price($USD) of the Costco item listed in 5 guesses
-                </p>
-                <p className={"mt-[10px]"}>
-                    <a className={"bg-red-600"}>RED</a> means that your guess is within 10% of the price
-                </p>
-                <p className={"mt-[10px] w-full border-gray-400 border-b-2 border-solid m-auto pb-5" }>
-                    <a className={"bg-yellow-300"}>YELLOW</a> means that your guess is over 10% of the price
-                </p>
-                <p className={"mt-5"}>
-                    DISCLOSURE!! This game was NOT invented by me but by @KermWasTaken and I merely adapted it to the discord format.
-                </p>
-                <p className={"mt-[10px]"}>
-                    All credits of ideas goes to the original creator and please try out the original game linked below
-                </p>
-                <p className={"mt-[10px]"}>
-                    https://costcodle.com/
-                </p>
-                <p className={"mt-[10px]"}>
-                    Press the X on the top right corner of this page or the Friends button on the header to close this screen and return to your game
-                </p>
+        <div className={"fixed w-full h-full top-0 left-0"}>
+            <div className="fixed w-full h-full bg-gray-100 pt-20 md:pt-0 top-[50px]">
+               <ExitButton toggle={toggle}/>
+                <div className={"wrap-break-word overflow-scroll text-center h-full w-full font-sans"}>
+                    <p className={"mt-[10px]"}>
+                        Guess the price($USD) of the Costco item listed in 5 guesses
+                    </p>
+                    <p className={"mt-[10px]"}>
+                        <a className={"bg-red-600"}>RED</a> means that your guess is within 10% of the price
+                    </p>
+                    <p className={"mt-[10px] w-full border-gray-400 border-b-2 border-solid m-auto pb-5" }>
+                        <a className={"bg-yellow-300"}>YELLOW</a> means that your guess is over 10% of the price
+                    </p>
+                    <p className={"mt-5"}>
+                        DISCLOSURE!! This game was NOT invented by me but by @KermWasTaken and I merely adapted it to the discord format.
+                    </p>
+                    <p className={"mt-[10px]"}>
+                        All credits of ideas goes to the original creator and please try out the original game linked below
+                    </p>
+                    <p className={"mt-[10px]"}>
+                        https://costcodle.com/
+                    </p>
+                </div>
             </div>
+
         </div>
     );
 }
