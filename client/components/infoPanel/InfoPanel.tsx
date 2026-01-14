8ea22a537be1e217eddb4ec3ea9c2cbe9ce75549
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-13 19:28:18 -0800

    Update InfoPanel.tsx

--- client/components/infoPanel/InfoPanel.tsx
+++ client/components/infoPanel/InfoPanel.tsx
@@ -8,7 +8,7 @@ function InfoPanel({toggle}:{toggle:() => void }) {
             <button className={"h-full w-full fixed opacity-20 bg-black"} onClick={toggle}/>
             <div className="fixed w-full bg-gray-100 pt-20 md:pt-0 top-[50px]">
                <ExitButton toggle={toggle}/>
-                <div className={"text-lg border-black border-3 border-solid fixed p-4 wrap-break-word overflow-scroll text-center font-serif icon:w-full md:w-1/2 icon:top-0 md:top-1/2 icon:left-0 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 bg-gray-200"}>
+                <div className={"border-black border-3 border-solid fixed p-4 wrap-break-word overflow-scroll text-center font-serif icon:w-full md:w-1/2 icon:top-0 md:top-1/2 icon:left-0 md:left-1/2 md:-translate-x-1/2 md:-translate-y-1/2 bg-gray-200"}>
                     <p className={"mt-[10px] text-lg font-bold"}>
                         Guess the price($USD) of the Costco item listed in 5 guesses
                     </p>
@@ -18,13 +18,13 @@ function InfoPanel({toggle}:{toggle:() => void }) {
                     <p className={"text-lg mt-[10px] w-full border-gray-400 border-b-2 border-solid m-auto pb-5" }>
                         <a className={"bg-yellow-300 font-bold"}>YELLOW</a> means that your guess is over 10% of the price
                     </p>
-                    <p className={"mt-5"}>
+                    <p className={"mt-5 text-sm"}>
                         DISCLOSURE!! This game was NOT invented by me but by @KermWasTaken and I merely adapted it to the discord format.
                     </p>
-                    <p className={"mt-[10px]"}>
+                    <p className={"mt-[10px] text-sm"}>
                         All credits of ideas goes to the original creator and please try out the original game linked below
                     </p>
-                    <p className={"mt-[10px]"}>
+                    <p className={"mt-[10px] text-sm"}>
                         https://costcodle.com/
                     </p>
                 </div>
