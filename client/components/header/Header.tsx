67d292d3a763ca35f5d08f4bd39470e866871942
Anderson Zhou <zhoua35@mcmaster.ca>
2026-01-13 16:52:46 -0800

    Update Header.tsx

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -9,11 +9,11 @@ function Header({toggleStat,toggleInfo}:{toggleStat:() => void ,toggleInfo:()=>v
     return (
         <div className="grid grid-cols-3 w-full xl:w-1/3 text-center m-auto font-costco p-2 pt-20 md:pt-0 bg-white border-b-1 border-solid border-black">
             <button onClick={toggleInfo}>
-                <img src={infoIcon} className="h-[10px] w-[10px] m-auto"/>
+                <img src={infoIcon} className="h-[30px] w-[30px] m-auto"/>
             </button>
             <p className={"text-3xl text-red-600 mt-1"}>Costcodle</p>
             <button onClick={toggleStat}>
-                <img src={statsIcon} className="h-[10px] w-[10px] m-auto"/>
+                <img src={statsIcon} className="h-[30px] w-[30px] m-auto"/>
             </button>
         </div>
     );
