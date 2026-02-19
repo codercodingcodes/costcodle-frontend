39818c3e9967af057b802f838cdbfe9674857469
Anderson Zhou <andersonzhouca@gmail.com>
2026-02-18 16:35:09 -0800

    added barchart

--- client/components/WinScreen/WinScreen.tsx
+++ client/components/WinScreen/WinScreen.tsx
@@ -4,12 +4,20 @@ import React from 'react';
 import loading from './membership-card-business.png';
 import {GameInfo, UserData} from "../../utils/types";
 import StatBar from "../stats/StatBar";
+import { BarChart } from '@mui/x-charts/BarChart';
 import ExitButton from "../exitButton/ExitButton";
 
 function WinScreen({guessCnt,gameData,toggle,users,self}:{guessCnt:number,gameData:GameInfo,toggle:()=>void,users:UserData[],self:string}) {
     return (
         <div className="top-0 w-full h-full text-center bg-gray-100 fixed animate-fade-in-fast">
             <StatBar users={users} self={self} toggle={toggle}/>
+            <BarChart
+                series={[
+                    {
+                        data: [2, 5, 3],
+                    },
+                ]}
+            />
             <p className={"absolute font-costco text-red-600 text-2xl md:text-4xl top-2/3 left-1/2 -translate-x-1/2 w-full"}>Congrats! You got today's item in {guessCnt} tries!</p>
             <p className={"absolute font-costco text-xl md:text-2xl top-4/5 left-1/2 -translate-x-1/2 w-full"}>Next game in : {24-Math.floor(gameData.time / 3600)} hrs</p>
         </div>
